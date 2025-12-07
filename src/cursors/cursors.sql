be96aa347003e6cab0f64bda6e3221e13b78e2b5
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-07 18:49:10 +0100

    Add commit and rollback handling in cursor SQL for student age update

--- src/cursors/cursors.sql
+++ src/cursors/cursors.sql
@@ -5,13 +5,18 @@ declare
     rowcoun number(3);
 begin
     update STUDENT set age = age + 2;
+    commit;
 
     if sql%notfound then
         DBMS_OUTPUT.PUT_LINE('no row effected');
     elsif sql%found then
         rowcoun := sql%rowcount;
         DBMS_OUTPUT.PUT_LINE(rowcoun || ' row effected');
-        commit ;
     end if;
 
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+
 end;
+/
