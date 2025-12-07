79d32394e1b31d816416a45a3063e53be80747b9
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-07 14:19:43 +0100

    Add example procedure to print first name and test it

--- src/procedures/prosedures.sql
+++ src/procedures/prosedures.sql
@@ -11,8 +11,22 @@ end;
 
 declare
     type list_of_numbers is varray(3) of number;
-    list1 list_of_numbers := list_of_numbers(2,4,6);
+    list1 list_of_numbers := list_of_numbers(2, 4, 6);
 begin
-  DBMS_OUTPUT.PUT_LINE(list1(1));
+    DBMS_OUTPUT.PUT_LINE(list1(1));
 end;
 /
+
+-- create an example procedure with one in parameter
+create or replace procedure print_first_name (name1 in varchar2) as
+begin
+    DBMS_OUTPUT.PUT_LINE(name1);
+end;
+/
+
+
+-- test that procedure
+
+begin
+    print_first_name('abdelhakk');
+end;
