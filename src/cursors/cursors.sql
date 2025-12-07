4e3a174b6dc5e72cab8f5cadaf38df4f39664f3f
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-07 19:05:18 +0100

    Enhance cursor SQL to log affected rows and handle errors during student age updates

--- src/cursors/cursors.sql
+++ src/cursors/cursors.sql
@@ -5,7 +5,15 @@ declare
     rowcoun number(3);
 begin
     update STUDENT set age = age + 2;
-    commit;
+
+    if sql%notfound then
+        DBMS_OUTPUT.PUT_LINE('no row effected');
+    elsif sql%found then
+        rowcoun := sql%rowcount;
+        DBMS_OUTPUT.PUT_LINE(rowcoun || ' row effected');
+    end if;
+
+    update student set age = 400 where age  = 500;
 
     if sql%notfound then
         DBMS_OUTPUT.PUT_LINE('no row effected');
@@ -16,6 +24,7 @@ begin
 
 EXCEPTION
     WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('error on executing');
         ROLLBACK;
 
 end;
