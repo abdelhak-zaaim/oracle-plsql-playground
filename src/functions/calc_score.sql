a9afea23ea277dde8d16181cbb760c2b6459a8e6
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-07 17:31:25 +0100

    Add example function to demonstrate summation within a block

--- src/functions/calc_score.sql
+++ src/functions/calc_score.sql
@@ -7,8 +7,25 @@ begin
 end;
 
 
-
 --test the function
 begin
     DBMS_OUTPUT.PUT_LINE(sun_of_numbers(23, 7));
+end;
+
+
+-- example of a function inside  Block
+
+declare
+    -- lets star declaring our variables before the actual function
+    nmbr1 number := 3;
+    nmbr2 number := 2;
+
+    function sum_of_tow(number1 in number, number2 in number) return number
+        is
+    begin
+        return number1 + number2;
+    end;
+
+begin
+    DBMS_OUTPUT.PUT_LINE(sum_of_tow(nmbr1, nmbr2));
 end;
