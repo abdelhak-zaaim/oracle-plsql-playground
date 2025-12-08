332491b0b05e0e65fecfa2ddba54d75d100d4802
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-08 15:02:45 +0100

    Add custom exception handling example in exceptions.sql

--- src/exceptions/exceptions.sql
+++ src/exceptions/exceptions.sql
@@ -3,10 +3,28 @@
 declare
     simple STUDENT%rowtype;
 begin
-    select * into  simple from STUDENT where id = 23; -- let's suppose this id not exist in our table
+    select * into simple from STUDENT where id = 23; -- let's suppose this id not exist in our table
 
- exception when NO_DATA_FOUND then
-    DBMS_OUTPUT.PUT_LINE('student not exist with that id');
+exception
+    when NO_DATA_FOUND then
+        DBMS_OUTPUT.PUT_LINE('student not exist with that id');
     when others then
         DBMS_OUTPUT.PUT_LINE('Error');
+end;
+
+
+-- so now lets declare our own exception and catch it
+
+declare
+    my_own_excp exception ;
+begin
+
+    if 1 > 0 then
+        raise my_own_excp;
+    end if;
+
+Exception
+    when my_own_excp then
+        DBMS_OUTPUT.PUT_LINE('my exception is raised lol!!');
+
 end;
