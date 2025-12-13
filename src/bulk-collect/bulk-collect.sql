0163877e7a1b76e18a6237c6b8e5677815854d84
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-13 17:16:55 +0100

    Add bulk collect example for updating student ages

--- src/bulk-collect/bulk-collect.sql
+++ src/bulk-collect/bulk-collect.sql
@@ -13,3 +13,31 @@ begin
             DBMS_OUTPUT.PUT_LINE(s_list(i).EMAIL);
         end loop;
 end;
+
+
+declare
+    TYPE table_s is table of student%rowtype index by pls_integer;
+    s_list table_s;
+begin
+
+    select * bulk collect into s_list from STUDENT;
+
+    for i in 1.. s_list.COUNT
+        loop
+            DBMS_OUTPUT.PUT_LINE(s_list(i).AGE);
+        end loop;
+
+    DBMS_OUTPUT.PUT_LINE('new ages - ---- --  -- -  -');
+
+    forall i in 1..s_list.COUNT
+            update STUDENT set age = age + 1 where ID = s_list(i).ID;
+
+
+    select * bulk collect into s_list from STUDENT;
+
+    for i in 1.. s_list.COUNT
+        loop
+            DBMS_OUTPUT.PUT_LINE(s_list(i).AGE);
+        end loop;
+
+end;
