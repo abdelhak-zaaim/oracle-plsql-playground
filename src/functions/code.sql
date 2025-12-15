ed2dbca0442bd326924c35fba535444ddb48074c
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2025-12-15 19:28:16 +0100

    Add function to retrieve student IDs by age

--- src/functions/code.sql
+++ src/functions/code.sql
@@ -1,11 +1,11 @@
 -- lets create a function that return the ids of student of specific age
 
-create or replace function get_students_ids_by_age(age STUDENT.AGE%type) return list_numbers
+create or replace function get_students_ids_by_age(s_age STUDENT.AGE%type) return list_numbers
 as
     TYPE list_numbers is table of number index by pls_integer;
     ids list_numbers;
 begin
-    select id bulk collect into ids from STUDENT where age = age;
+    select id bulk collect into ids from STUDENT where age = s_age;
     return ids;
 
 exception
